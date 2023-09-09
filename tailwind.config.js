e8b854b08613e062c726a5b466c38a3fb63f0224
ratulraihan <56727230+ratulraihan@users.noreply.github.com>
2023-09-09 15:11:09 +0600

    container padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
     extend: {},
     container: {
       center: true,
+      padding: {
+        DEFAULT: "1rem",
+        md: "2rem",
+        lg: "3rem",
+        xl: "4rem",
+        "2xl": "5rem",
+      },
     },
   },
   plugins: [],
