7fcf04af5c38400b052f4f3190b0ce70c77666ea
ratulraihan <56727230+ratulraihan@users.noreply.github.com>
2023-09-21 16:32:51 +0600

    responsive about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -4,17 +4,17 @@ export default function About() {
     return (
         <div>
             <div className="bg-[url('/image/about.jpg')] bg-center bg-cover ">
-                <h1 className="container py-64 text-6xl font-semibold tracking-widest text-white ">WHO ARE WE ?</h1>
+                <h1 className="container py-32 text-6xl font-semibold tracking-widest text-center text-white lg:py-64 lg:text-left ">WHO ARE WE?</h1>
 
             </div>
             <div className="container ">
-                <div className="py-14">
-                    <h2 className="p-20 text-5xl font-semibold text-center">We have great idea & Interior Design</h2>
-                    <p className="w-1/2 text-2xl font-medium">
+                <div className="py-4 lg:py-14">
+                    <h2 className="p-4 text-3xl font-semibold text-center lg:p-20 lg:text-5xl">We have great idea & Interior Design</h2>
+                    <p className="text-2xl font-medium lg:w-1/2">
                         The Digimax interior design company is a company that provides interior design services for homes, offices, apartments, and others. We provide the best interior design services for you. We have a team that is experienced in the field of interior.
                     </p>
                 </div>
-                <div className="flex items-center gap-x-8">
+                <div className="items-center lg:flex gap-x-8">
                     <div className="w-full">
                         <Image src="/image/gallery1123.jpg" width={700} height={700} alt="" className="" />
                     </div>
@@ -34,11 +34,11 @@ export default function About() {
 
                     </div>
                 </div>
-                <div className="py-20">
-                    <div className="py-8">
+                <div className="lg:py-20">
+                    <div className="pt-8 pb-4">
                         <h1 className="text-4xl font-bold tracking-wider text-center">TEAM</h1>
                     </div>
-                    <div className="grid grid-cols-3 gap-20 py-8">
+                    <div className="grid gap-20 py-8 lg:grid-cols-3">
                         <div className="border-2 border-gray-500 ">
                             <div className="-m-0.5 p-4 text-center transition hover:-translate-y-3 hover:-translate-x-3 aspect-square bg-zinc-100 ">
                                 <Image src="/image/profile2.jpg" width={200} height={200} alt="" className="mx-auto rounded-full " />
